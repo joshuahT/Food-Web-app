e9a3208b5a0eda9d899cc1f7741884d509b03ea9
Junqiang Chen <89885929+junqiang2021@users.noreply.github.com>
2022-04-18 21:48:08 -0400

    Update Recepies.js

--- src/pages/Recepies.js
+++ src/pages/Recepies.js
@@ -1,7 +1,91 @@
-export default function Recepies() {
+import '../styles/App.css';
+import React, { useEffect, useState } from 'react';
+import Axios from "axios";
+import Dialog from '@mui/material/Dialog';
+import DialogContent from '@mui/material/DialogContent';
+import DialogActions from '@mui/material/DialogActions';
+import DialogTitle from '@mui/material/DialogTitle';
+import foodLogo from '../images/food-app-icon.png'
+import {
+  RecipeContainer,
+  RecipeListContainer,
+  CoverImage,
+  RecipeName,
+  IngredientsText,
+  SeeMoreText,
+  Placeholder,
+  SaveButton
+} from "../components/RecipeComponent";
+
+
+const RecipeComponent = (props) => {
+  const [popUp, setPopUp] = useState(false);
+  const { recipeObj } = props;
   return (
-    <div className="recepies">
-      <h1>Top Recipe Page</h1>
+    <>
+      <Dialog open={popUp}>
+        <DialogTitle>Ingredients</DialogTitle>
+        <DialogContent>
+          <table>
+            <tbody>
+              {recipeObj.ingredients.map((ingredientObj) => (
+                <tr>
+                  <td>{ingredientObj.text}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </DialogContent>
+        <DialogActions>
+          <SeeMoreText onClick={() => setPopUp("")}>Close</SeeMoreText>
+        </DialogActions>
+      </Dialog>
+      <RecipeContainer>
+        <CoverImage src={recipeObj.image} />
+        <RecipeName>{recipeObj.label}</RecipeName>
+        <IngredientsText onClick={() => setPopUp(true)}>Ingredients</IngredientsText>
+        <SeeMoreText onClick={() => window.open(recipeObj.url)}>See complete Recipe</SeeMoreText>
+        <SaveButton onClick={() => Save(recipeObj)}>Save</SaveButton>
+      </RecipeContainer>
+    </>
+  );
+};
+function Save(x) {
+  const saveObj = { label: x.label, ingredient: x.ingredientLines }
+  console.log(saveObj)
+  fetch('/update', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(saveObj)
+  });
+  alert("Saved to profile!")
+}
+function Recepies() {
+  const [reciptList, updateReciptList] = useState([]);
+  const fetchRecipe = async () => {
+    const response = await Axios.get(
+      `https://api.edamam.com/api/recipes/v2?type=public&q=dish&app_id=${process.env.REACT_APP_APP_ID}&app_key=${process.env.REACT_APP_APP_KEY}&random=true`
+    );
+    updateReciptList(response.data.hits);
+  };
+
+  useEffect(() => {
+    fetchRecipe()
+  }, [])
+  return (
+    <div className="recepies" align="center">
+      <h1>Random Recipe Page</h1>
+      <br></br>
+      <RecipeListContainer>
+        {reciptList.length ?
+          reciptList.map((recipeObj) => (
+            <RecipeComponent recipeObj={recipeObj.recipe} />
+          )) : <Placeholder src={foodLogo} />}
+      </RecipeListContainer>
     </div>
   )
 }
+
+export default Recepies;
